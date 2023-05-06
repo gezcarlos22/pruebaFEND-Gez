c76aa544cfdd14ca4e1dcbb67e1a50c22f422068
gezcarlos22 <ceg290698@gmail.com>
2023-05-06 05:40:27 -0300

    Back End

---
+++ Portafolio/Back End/src/main/java/com/portafolio/Gez/Security/Controller/Mensaje.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portafolio.Gez.Security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    
+    //constructor
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    //getter y setter
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
